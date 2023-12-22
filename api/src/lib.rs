103ed72f62e8abaa41ce91c0e7e49ce68755e19d
Tom Christian <tom@sparkgeo.com>
2023-12-21 16:17:07 -0800

    Refactored for dedicated get_image function

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -22,15 +22,7 @@ async fn handle_api(req: Request) -> anyhow::Result<impl IntoResponse> {
     println!("Handling request to {:?}", req.header("spin-full-url"));
     match api_from_request(&req).await {
         ApiBehaviour::Get(identifier) => {
-            let img_href = format!("{:}{:}_{:?}.jp2", IMG_BASE_HREF, identifier.image_id, identifier.band);
-            let response: Response = send(Request::get(img_href)).await.unwrap();            
-            let mut builder = http::Response::builder().status(*response.status());
-            for header_entry in response.headers() {
-                if let Some(header_value_str) = header_entry.1.as_str() {
-                    builder = builder.header(header_entry.0, header_value_str);
-                }
-            }
-            Ok(builder.body(response.body().to_vec()).unwrap())
+            Ok(get_image(&identifier).await)
         },
         _ => Ok(http::Response::builder()
             .status(200)
@@ -62,4 +54,16 @@ async fn api_from_request<'req>(req: &'req Request) -> ApiBehaviour<'req> {
             ApiBehaviour::NotFound
         }
     }
+}
+
+async fn get_image<'req>(identifier: &'req BandIdentifier<'req>) -> http::Response<Vec<u8>> {
+    let img_href = format!("{:}{:}_{:?}.jp2", IMG_BASE_HREF, identifier.image_id, identifier.band);
+    let response: Response = send(Request::get(img_href)).await.unwrap();            
+    let mut builder = http::Response::builder().status(*response.status());
+    for header_entry in response.headers() {
+        if let Some(header_value_str) = header_entry.1.as_str() {
+            builder = builder.header(header_entry.0, header_value_str);
+        }
+    }
+    builder.body(response.body().to_vec()).unwrap()
 }
